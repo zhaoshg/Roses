d03406ffa72a034cdb15337f52d297a3d58bd5bf
fengshuonan <sn93@qq.com>
2023-06-27 18:04:18 +0800

    【7.6.0】【dict】删除没用的类

--- kernel-s-dict/dict-business/src/main/java/cn/stylefeng/roses/kernel/dict/modular/mapper/DictMapper.java
+++ kernel-s-dict/dict-business/src/main/java/cn/stylefeng/roses/kernel/dict/modular/mapper/DictMapper.java
@@ -24,14 +24,8 @@
  */
 package cn.stylefeng.roses.kernel.dict.modular.mapper;
 
-import cn.stylefeng.roses.kernel.dict.api.pojo.dict.request.ParentIdsUpdateRequest;
 import cn.stylefeng.roses.kernel.dict.modular.entity.SysDict;
-import cn.stylefeng.roses.kernel.dict.modular.pojo.request.DictRequest;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 /**
  * 字典详情的管理
