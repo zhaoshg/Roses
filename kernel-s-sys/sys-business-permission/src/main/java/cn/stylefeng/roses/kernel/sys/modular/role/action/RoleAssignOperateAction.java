22ba1d26827bd57282b2551e89027ca1e6b3e38a
fengshuonan <sn93@qq.com>
2023-07-01 21:21:48 +0800

    【7.6.0】【role】调整角色绑定权限，不返回信息，前端直接渲染

--- kernel-s-sys/sys-business-permission/src/main/java/cn/stylefeng/roses/kernel/sys/modular/role/action/RoleAssignOperateAction.java
+++ kernel-s-sys/sys-business-permission/src/main/java/cn/stylefeng/roses/kernel/sys/modular/role/action/RoleAssignOperateAction.java
@@ -2,9 +2,6 @@ package cn.stylefeng.roses.kernel.sys.modular.role.action;
 
 import cn.stylefeng.roses.kernel.sys.modular.role.enums.PermissionNodeTypeEnum;
 import cn.stylefeng.roses.kernel.sys.modular.role.pojo.request.RoleBindPermissionRequest;
-import cn.stylefeng.roses.kernel.sys.modular.role.pojo.response.RoleBindPermissionItem;
-
-import java.util.List;
 
 /**
  * 角色绑定权限操作的接口
@@ -26,10 +23,9 @@ public interface RoleAssignOperateAction {
      * 执行角色绑定权限的过程，根据不同的点击类型，执行不同的操作过程
      *
      * @param roleBindPermissionRequest 角色绑定权限的参数
-     * @return 返回节点绑定后的执行结果，返回的是当前操作节点的子集的所有
      * @author fengshuonan
      * @since 2023/6/13 22:17
      */
-    List<RoleBindPermissionItem> doOperateAction(RoleBindPermissionRequest roleBindPermissionRequest);
+    void doOperateAction(RoleBindPermissionRequest roleBindPermissionRequest);
 
 }
