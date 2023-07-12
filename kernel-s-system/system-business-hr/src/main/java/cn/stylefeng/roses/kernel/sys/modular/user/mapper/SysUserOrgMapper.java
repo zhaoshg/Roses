e14905d2398e50857647d68b9e9ea67d1fedbc7e
fengshuonan <sn93@qq.com>
2023-07-12 21:56:41 +0800

    【8.0】【system】更新user org的批量插入方法

--- kernel-s-system/system-business-hr/src/main/java/cn/stylefeng/roses/kernel/sys/modular/user/mapper/SysUserOrgMapper.java
+++ kernel-s-system/system-business-hr/src/main/java/cn/stylefeng/roses/kernel/sys/modular/user/mapper/SysUserOrgMapper.java
@@ -1,7 +1,7 @@
 package cn.stylefeng.roses.kernel.sys.modular.user.mapper;
 
+import cn.stylefeng.roses.kernel.db.mp.injector.CustomBaseMapper;
 import cn.stylefeng.roses.kernel.sys.modular.user.entity.SysUserOrg;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
  * 用户组织机构关联 Mapper 接口
@@ -9,6 +9,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author fengshuonan
  * @date 2023/06/10 21:26
  */
-public interface SysUserOrgMapper extends BaseMapper<SysUserOrg> {
+public interface SysUserOrgMapper extends CustomBaseMapper<SysUserOrg> {
 
 }
