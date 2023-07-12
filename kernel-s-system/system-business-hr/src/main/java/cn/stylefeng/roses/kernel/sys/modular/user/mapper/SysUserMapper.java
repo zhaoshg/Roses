269f18926727b641f4a54b2a80b7a1dfa39c7231
fengshuonan <sn93@qq.com>
2023-07-12 22:07:35 +0800

    【8.0】【system】更新mapper批量插入

--- kernel-s-system/system-business-hr/src/main/java/cn/stylefeng/roses/kernel/sys/modular/user/mapper/SysUserMapper.java
+++ kernel-s-system/system-business-hr/src/main/java/cn/stylefeng/roses/kernel/sys/modular/user/mapper/SysUserMapper.java
@@ -1,7 +1,7 @@
 package cn.stylefeng.roses.kernel.sys.modular.user.mapper;
 
+import cn.stylefeng.roses.kernel.db.mp.injector.CustomBaseMapper;
 import cn.stylefeng.roses.kernel.sys.modular.user.entity.SysUser;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
  * 系统用户 Mapper 接口
@@ -9,6 +9,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author fengshuonan
  * @date 2023/06/10 21:26
  */
-public interface SysUserMapper extends BaseMapper<SysUser> {
+public interface SysUserMapper extends CustomBaseMapper<SysUser> {
 
 }
