8a0485b07a15c5b0c404533cbaca4641718059d1
fengshuonan <sn93@qq.com>
2023-07-18 23:14:49 +0800

    【8.0】【sys】更新返回值说明

--- kernel-s-system/system-api/src/main/java/cn/stylefeng/roses/kernel/sys/api/UserDataScopeApi.java
+++ kernel-s-system/system-api/src/main/java/cn/stylefeng/roses/kernel/sys/api/UserDataScopeApi.java
@@ -22,8 +22,13 @@ public interface UserDataScopeApi {
 
     /**
      * 获取当前用户拥有的所有组织机构id列表
+     * <p>
+     * 返回值说明：
+     * 1. 可以返回null或者空，代表用户有所有权限，也就是全部数据
+     * 2. 返回带有userId或者orgId的选项，代表用户有这些人或者这些机构的权限
+     * 3. 返回带有负数（例如：-1）的数组，则代表用户没有任何权限
      *
-     * @return null或者空数组，代表用户没有访问权限，其他情况则代表用户拥有的组织机构id访问权限
+     * @return 用户拥有的数据范围，userId或者orgId
      * @author fengshuonan
      * @since 2023/7/18 22:54
      */
