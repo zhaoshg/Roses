8aa0f33bf373ce61b0b90b7ba79bead08ed15387
fengshuonan <sn93@qq.com>
2023-07-21 13:53:02 +0800

    【8.0】【log】更新bean配置

--- kernel-d-log/log-business-requestapi/src/main/java/cn/stylefeng/roses/kernel/log/requestapi/service/impl/SysLogServiceImpl.java
+++ kernel-d-log/log-business-requestapi/src/main/java/cn/stylefeng/roses/kernel/log/requestapi/service/impl/SysLogServiceImpl.java
@@ -45,7 +45,6 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
@@ -57,7 +56,6 @@ import java.util.List;
  * @author luojie
  * @since 2020/11/2 17:45
  */
-@Service
 public class SysLogServiceImpl extends ServiceImpl<SysLogMapper, SysLog> implements SysLogService {
 
     @Override
