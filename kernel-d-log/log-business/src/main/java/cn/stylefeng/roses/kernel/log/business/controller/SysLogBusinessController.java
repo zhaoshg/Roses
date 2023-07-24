e5682488241fb8a2c5ea0b52a11c71cc69712aa7
fengshuonan <sn93@qq.com>
2023-07-24 13:36:55 +0800

    【8.0】【log】删掉没用的接口

--- kernel-d-log/log-business/src/main/java/cn/stylefeng/roses/kernel/log/business/controller/SysLogBusinessController.java
+++ kernel-d-log/log-business/src/main/java/cn/stylefeng/roses/kernel/log/business/controller/SysLogBusinessController.java
@@ -8,13 +8,9 @@ import cn.stylefeng.roses.kernel.rule.pojo.response.ResponseData;
 import cn.stylefeng.roses.kernel.rule.pojo.response.SuccessResponseData;
 import cn.stylefeng.roses.kernel.scanner.api.annotation.ApiResource;
 import cn.stylefeng.roses.kernel.scanner.api.annotation.GetResource;
-import cn.stylefeng.roses.kernel.scanner.api.annotation.PostResource;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * 业务日志记录控制器
@@ -28,64 +24,6 @@ public class SysLogBusinessController {
 
     @Resource
     private SysLogBusinessService sysLogBusinessService;
-
-    /**
-     * 添加
-     *
-     * @author fengshuonan
-     * @date 2023/07/21 19:02
-     */
-    @PostResource(name = "添加", path = "/sysLogBusiness/add")
-    public ResponseData<SysLogBusiness> add(@RequestBody @Validated(SysLogBusinessRequest.add.class) SysLogBusinessRequest sysLogBusinessRequest) {
-        sysLogBusinessService.add(sysLogBusinessRequest);
-        return new SuccessResponseData<>();
-    }
-
-    /**
-     * 删除
-     *
-     * @author fengshuonan
-     * @date 2023/07/21 19:02
-     */
-    @PostResource(name = "删除", path = "/sysLogBusiness/delete")
-    public ResponseData<?> delete(@RequestBody @Validated(SysLogBusinessRequest.delete.class) SysLogBusinessRequest sysLogBusinessRequest) {
-        sysLogBusinessService.del(sysLogBusinessRequest);
-        return new SuccessResponseData<>();
-    }
-
-    /**
-     * 编辑
-     *
-     * @author fengshuonan
-     * @date 2023/07/21 19:02
-     */
-    @PostResource(name = "编辑", path = "/sysLogBusiness/edit")
-    public ResponseData<?> edit(@RequestBody @Validated(SysLogBusinessRequest.edit.class) SysLogBusinessRequest sysLogBusinessRequest) {
-        sysLogBusinessService.edit(sysLogBusinessRequest);
-        return new SuccessResponseData<>();
-    }
-
-    /**
-     * 查看详情
-     *
-     * @author fengshuonan
-     * @date 2023/07/21 19:02
-     */
-    @GetResource(name = "查看详情", path = "/sysLogBusiness/detail")
-    public ResponseData<SysLogBusiness> detail(@Validated(SysLogBusinessRequest.detail.class) SysLogBusinessRequest sysLogBusinessRequest) {
-        return new SuccessResponseData<>(sysLogBusinessService.detail(sysLogBusinessRequest));
-    }
-
-    /**
-     * 获取列表
-     *
-     * @author fengshuonan
-     * @date 2023/07/21 19:02
-     */
-    @GetResource(name = "获取列表", path = "/sysLogBusiness/list")
-    public ResponseData<List<SysLogBusiness>> list(SysLogBusinessRequest sysLogBusinessRequest) {
-        return new SuccessResponseData<>(sysLogBusinessService.findList(sysLogBusinessRequest));
-    }
 
     /**
      * 获取列表（带分页）
