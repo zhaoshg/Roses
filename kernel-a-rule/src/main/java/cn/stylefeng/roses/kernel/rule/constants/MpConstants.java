cbeff120e541d8255cc317ed7b0fb6e2cd445785
fengshuonan <sn93@qq.com>
2023-08-03 17:30:52 +0800

    【8.0】【rule】更新mp的typehandler配置，增加别的json解析支持

--- kernel-a-rule/src/main/java/cn/stylefeng/roses/kernel/rule/constants/MpConstants.java
+++ kernel-a-rule/src/main/java/cn/stylefeng/roses/kernel/rule/constants/MpConstants.java
@@ -9,8 +9,24 @@ package cn.stylefeng.roses.kernel.rule.constants;
 public class MpConstants {
 
     /**
+     * jackson模式
+     * <p>
      * 一般用在手动使用LambdaUpdateWrapper时候，手动更新json字段，需要手动指定typeHandler
      */
-    public static final String TYPE_HANDLER_STRING = "typeHandler=com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler";
+    public static final String TYPE_HANDLER_STRING_JACKSON = "typeHandler=com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler";
+
+    /**
+     * gson模式
+     * <p>
+     * 一般用在手动使用LambdaUpdateWrapper时候，手动更新json字段，需要手动指定typeHandler
+     */
+    public static final String TYPE_HANDLER_STRING_GSON = "typeHandler=com.baomidou.mybatisplus.extension.handlers.GsonTypeHandler";
+
+    /**
+     * fastjson模式
+     * <p>
+     * 一般用在手动使用LambdaUpdateWrapper时候，手动更新json字段，需要手动指定typeHandler
+     */
+    public static final String TYPE_HANDLER_STRING_FASTJSON = "typeHandler=com.baomidou.mybatisplus.extension.handlers.FastjsonTypeHandler";
 
 }
