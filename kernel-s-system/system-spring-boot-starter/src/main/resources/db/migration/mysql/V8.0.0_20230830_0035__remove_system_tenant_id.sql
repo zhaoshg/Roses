4fd0a89dc5d81537adcf90d6b7ef62fd56097c7b
fengshuonan <sn93@qq.com>
2023-08-31 11:51:41 +0800

    【8.0】【tenant】更新默认租户的数据

--- kernel-s-system/system-spring-boot-starter/src/main/resources/db/migration/mysql/V8.0.0_20230830_0035__remove_system_tenant_id.sql
+++ kernel-s-system/system-spring-boot-starter/src/main/resources/db/migration/mysql/V8.0.0_20230830_0035__remove_system_tenant_id.sql
@@ -17,3 +17,12 @@ ALTER TABLE `sys_role_menu_options` DROP COLUMN `tenant_id`;
 ALTER TABLE `sys_hr_org_approver` DROP COLUMN `tenant_id`;
 
 ALTER TABLE `sys_portal_user_app` DROP COLUMN `tenant_id`;
+
+-- 增加默认租户数据，默认租户id是1
+update sys_user set tenant_id = 1 where tenant_id is null;
+
+update sys_role set tenant_id = 1 where tenant_id is null;
+
+update sys_hr_position set tenant_id = 1 where tenant_id is null;
+
+update sys_hr_organization set tenant_id = 1 where tenant_id is null;
