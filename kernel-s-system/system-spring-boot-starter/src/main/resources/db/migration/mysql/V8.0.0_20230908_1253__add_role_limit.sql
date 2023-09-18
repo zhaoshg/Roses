aa4205efe37550293630c6cb919403e4837702f9
fengshuonan <sn93@qq.com>
2023-09-18 23:34:56 +0800

    【8.0】【role】更新sql的字段长度

--- kernel-s-system/system-spring-boot-starter/src/main/resources/db/migration/mysql/V8.0.0_20230908_1253__add_role_limit.sql
+++ kernel-s-system/system-spring-boot-starter/src/main/resources/db/migration/mysql/V8.0.0_20230908_1253__add_role_limit.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `sys_role_limit`  (
-  `role_limit_id` bigint(0) NOT NULL COMMENT '主键',
-  `role_id` bigint(0) NOT NULL COMMENT '角色id',
-  `limit_type` tinyint(0) NOT NULL COMMENT '角色限制类型：1-角色可分配的菜单，2-角色可分配的功能',
-  `business_id` bigint(0) NOT NULL COMMENT '业务id，为菜单id或菜单功能id',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `create_user` bigint(0) NULL DEFAULT NULL COMMENT '创建人',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
-  `update_user` bigint(0) NULL DEFAULT NULL COMMENT '修改人',
+  `role_limit_id` bigint NOT NULL COMMENT '主键',
+  `role_id` bigint NOT NULL COMMENT '角色id',
+  `limit_type` tinyint NOT NULL COMMENT '角色限制类型：1-角色可分配的菜单，2-角色可分配的功能',
+  `business_id` bigint NOT NULL COMMENT '业务id，为菜单id或菜单功能id',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `create_user` bigint NULL DEFAULT NULL COMMENT '创建人',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  `update_user` bigint NULL DEFAULT NULL COMMENT '修改人',
   PRIMARY KEY (`role_limit_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '角色权限限制' ROW_FORMAT = Dynamic;
