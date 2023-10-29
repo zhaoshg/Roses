dfe82b1209be98316803d31fb82888f53fd5079c
fengshuonan <sn93@qq.com>
2023-10-29 18:08:20 +0800

    【8.0.1】【system】更新系统管理模块的最大排序字段配置

--- kernel-s-system/system-api/src/main/java/cn/stylefeng/roses/kernel/sys/api/maxsort/SystemMaxSortFieldConfig.java
+++ kernel-s-system/system-api/src/main/java/cn/stylefeng/roses/kernel/sys/api/maxsort/SystemMaxSortFieldConfig.java
@@ -19,7 +19,14 @@ public class SystemMaxSortFieldConfig implements MaxSortCollectorApi {
     @Override
     public Map<String, MaxCountConfig> createMaxSortConfigs() {
         HashMap<String, MaxCountConfig> stringMaxCountConfigHashMap = new HashMap<>();
-        stringMaxCountConfigHashMap.put("user", new MaxCountConfig("sys_user", "user_sort"));
+        stringMaxCountConfigHashMap.put("SYSTEM_BASE_APP", new MaxCountConfig("sys_app", "app_sort"));
+        stringMaxCountConfigHashMap.put("SYSTEM_BASE_DICT_TYPE", new MaxCountConfig("sys_dict_type", "dict_type_sort"));
+        stringMaxCountConfigHashMap.put("SYSTEM_BASE_DICT", new MaxCountConfig("sys_dict", "dict_sort"));
+        stringMaxCountConfigHashMap.put("SYSTEM_HR_ORGANIZATION", new MaxCountConfig("sys_hr_organization", "org_sort"));
+        stringMaxCountConfigHashMap.put("SYSTEM_HR_POSITION", new MaxCountConfig("sys_hr_position", "position_sort"));
+        stringMaxCountConfigHashMap.put("SYSTEM_BASE_MENU", new MaxCountConfig("sys_menu", "menu_sort"));
+        stringMaxCountConfigHashMap.put("SYSTEM_BASE_ROLE", new MaxCountConfig("sys_role", "role_sort"));
+        stringMaxCountConfigHashMap.put("SYSTEM_HR_USER", new MaxCountConfig("sys_user", "user_sort"));
         return stringMaxCountConfigHashMap;
     }
 
