715ab551a7224929ddec2ab912a5edec67ae96af
fengshuonan <sn93@qq.com>
2024-01-17 15:30:58 +0800

    【8.1.0】【role】更新用户角色表增加两个字段，一个角色类型，一个角色所属公司id

--- kernel-s-system/system-spring-boot-starter/src/main/resources/db/migration/mysql/V8.1.0_20240115_2308__multi_org_role.sql
+++ kernel-s-system/system-spring-boot-starter/src/main/resources/db/migration/mysql/V8.1.0_20240115_2308__multi_org_role.sql
@@ -4,4 +4,5 @@ ADD COLUMN `role_type` tinyint NOT NULL DEFAULT 10 COMMENT '角色类型：10-�
 ADD COLUMN `role_company_id` bigint NULL COMMENT '角色所属公司id，当角色类型为20时传此值' AFTER `role_type`;
 
 ALTER TABLE `sys_user_role`
-ADD COLUMN `role_company_id` bigint NULL COMMENT '角色所属公司id' AFTER `role_id`;
+ADD COLUMN `role_type` tinyint NOT NULL DEFAULT 10 COMMENT '角色类型：10-系统角色，20-公司角色' AFTER `role_id`,
+ADD COLUMN `role_company_id` bigint NULL DEFAULT NULL COMMENT '角色所属公司id' AFTER `role_type`;
